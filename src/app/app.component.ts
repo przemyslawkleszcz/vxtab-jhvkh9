b5a970f8f07fee9b896eea1e359c1ce6e629249c
Przemyslaw Kleszcz <kleszcz@mail.com>
2022-02-23 02:21:49 +0100

    new menu item - login to dropbox added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { DropboxService } from './dropbox.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private access_token: string;
+
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public dropbox: DropboxService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    platform.ready().then(() => {
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      this.activatedRoute.fragment.subscribe((params) => {
+        if (params == null) return;
+
+        let fragments = params.split('&');
+        let fragment = {};
+        fragments.forEach((x) => {
+          let y = x.split('=');
+          fragment[y[0]] = y[1];
+        });
+
+        console.log(fragment);
+
+        this.access_token = fragment['access_token'];
+      });
+    });
+  }
+  ngOnInit(): void {}
+
+  loginToDropbox() {
+    this.dropbox.login();
+  }
+
   public appPages = [
     { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
 }
